7d094d0f355ff1df33d0c997529095a22b70a9c8
Pauline <960707@live.se>
2017-11-28 13:32:02 +0100

    nytt

--- js.js
+++ js.js
@@ -4,21 +4,21 @@ var menu = document.getElementById('menu');
 
 menu.addEventListener('transitionend', function() {
 	console.log('Transition done');
-})
+});
 
 h.addEventListener('click', function(){
 	document.getElementById('navi').classList.toggle('toggled');
 	h.style.display = "none"; 
 	c.style.display = "block";
 	 
-})
+});
 
 c.addEventListener('click', function(){
 	document.getElementById('navi').classList.toggle('toggled');
 	c.style.display = "none";
 	h.style.display = "block";
 	
-})
+});
 
 function calc() {
 	var rSum = 0;
