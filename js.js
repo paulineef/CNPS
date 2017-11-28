5e8ad77a05d6f2ec0d30cdeb057c49c6f7e103a9
Pauline <960707@live.se>
2017-11-28 12:36:19 +0100

    senaste

--- js.js
+++ js.js
@@ -1,22 +1,23 @@
 var h = document.getElementById('hamburger');
 var c = document.getElementById('cross');
-var menu =document.getElementById("menu"); 
+var menu = document.getElementById('menu');
 
-// menu.addEventListener('transitionend', function() {
-// 	console.log('Transition done');
-// })
+menu.addEventListener('transitionend', function() {
+	console.log('Transition done');
+})
 
 h.addEventListener('click', function(){
+	document.getElementById('navi').classList.toggle('toggled');
 	h.style.display = "none"; 
 	c.style.display = "block";
-	menu.style.display = "block";
-	menu.style.transition = "all 2s";   
+	 
 })
 
 c.addEventListener('click', function(){
+	document.getElementById('navi').classList.toggle('toggled');
 	c.style.display = "none";
 	h.style.display = "block";
-	menu.style.display = "none";
+	
 })
 
 function calc() {
